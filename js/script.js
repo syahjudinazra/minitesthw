2e797bf12e6b74c2fa21e5bdf320c4dc6f6c06e6
iMin <syahjudinazra@gmail.com>
2023-11-16 09:31:14 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -2,16 +2,12 @@ function printNumbers() {
   for (let i = 1; ; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("TigaLima");
-      document.getElementById("output").innerHTML += "TigaLima <br>";
     } else if (i % 3 === 0) {
       console.log("Tiga");
-      document.getElementById("output").innerHTML += "Tiga <br>";
     } else if (i % 5 === 0) {
       console.log("Lima");
-      document.getElementById("output").innerHTML += "Lima <br>";
     } else {
       console.log(i);
-      document.getElementById("output").innerHTML += i + " <br>";
     }
   }
 }
